f7c5d8206c0a4ed077d1f72bbb018539a4318eb5
woabc <yjhy88@gmail.com>
2025-06-21 16:58:51 +0800

    页眉增加logo1

--- src/pages/home/components/SiteNavBar.vue
+++ src/pages/home/components/SiteNavBar.vue
@@ -97,7 +97,7 @@ onMounted(() => {
   <section
     class="site-navbar-sidebar pb-12 text-16" :class="{ 'is-open': settingStore.isSideNavOpen }"
   >
-    <div v-if="isMobile && settingStore.isSideNavOpen" class="sidebar-logo-container flex items-center justify-center py-7 p-4">
+    <div v-if="!isMobile || (isMobile && settingStore.isSideNavOpen)" class="sidebar-logo-container flex items-center justify-center py-7 p-4">
       <img src="/logo.jpg" alt="Logo" class="w-auto h-40">
     </div>
 
