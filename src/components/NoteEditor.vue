37b75263361b2ace0582f6b2d3bed66cb4c5c66d
woabc <yjhy88@gmail.com>
2025-09-15 16:23:11 -0700

    输入框加宽

--- src/components/NoteEditor.vue
+++ src/components/NoteEditor.vue
@@ -564,7 +564,7 @@ defineExpose({ reset: triggerResize })
   min-height: 40px;
   max-height: 48vh;
   overflow-y: auto;
-  padding: 16px 16px 8px 16px;
+  padding: 16px 8px 8px 16px;
   border: none;
   background-color: transparent;
   color: inherit;
