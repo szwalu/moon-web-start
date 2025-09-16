09f1bf9b32f2eb5feeeb74071999220dbc16f02d
woabc <yjhy88@gmail.com>
2025-09-15 20:29:25 -0700

    测试

--- src/components/NoteEditor.vue
+++ src/components/NoteEditor.vue
@@ -574,7 +574,7 @@ defineExpose({ reset: triggerResize })
 .editor-textarea {
   width: 100%;
   min-height: 40px;
-  max-height: 70vh;
+  max-height: 50vh;
   overflow-y: auto;
   padding: 16px 8px 8px 16px;
   border: none;
