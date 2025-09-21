887c7e959d38158f5c1e59f3f2e1a354987718e0
woabc <yjhy88@gmail.com>
2025-09-21 00:01:51 -0700

    输入框滚动

--- src/components/NoteEditor.vue
+++ src/components/NoteEditor.vue
@@ -995,7 +995,7 @@ defineExpose({ reset: triggerResize })
 .editor-textarea {
   width: 100%;
   min-height: 40px;
-  max-height: 48vh;
+  max-height: 50vh;
   overflow-y: auto;
   padding: 8px 8px 1px 16px;
   border: none;
