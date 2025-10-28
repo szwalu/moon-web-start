ab5addc79d0e9be2a9dba4daab80e86408d19029
woabc <yjhy88@gmail.com>
2025-10-28 15:13:10 -0700

    恢复

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli'
-
-const config: CapacitorConfig = {
-  appId: 'com.kunmingli.moonnotes',
-  appName: '云笔记',
-  webDir: 'dist',
-}
-
-export default config
