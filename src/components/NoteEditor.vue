92e5f6bff1bfb580ac1d9fa03cf23aac8043b3c2
woabc <yjhy88@gmail.com>
2025-11-01 14:14:34 -0700

    增加图片储存

--- src/components/NoteEditor.vue
+++ src/components/NoteEditor.vue
@@ -1577,7 +1577,6 @@ function handleBeforeInput(e: InputEvent) {
             @pointerdown="onPickImageSync"
             @click="onPickImageSync"
           >
-            >
             <!-- Image icon -->
             <svg class="icon-20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
               <rect x="3" y="4" width="18" height="16" rx="2.5" stroke="currentColor" stroke-width="1.6" />
