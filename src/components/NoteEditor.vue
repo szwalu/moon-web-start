a3e9bdb6832a1ef6352d3232300b802599cec72e
woabc <yjhy88@gmail.com>
2025-11-04 16:05:51 -0800

    测试加号

--- src/components/NoteEditor.vue
+++ src/components/NoteEditor.vue
@@ -1771,7 +1771,7 @@ function handleBeforeInput(e: InputEvent) {
 .editor-textarea {
   width: 100%;
   min-height: 40px;
-  max-height: 56vh;
+  max-height: var(--editor-max-h, 56vh);;
   overflow-y: auto;
   padding: 12px 8px 8px 16px;
   border: none;
@@ -1971,7 +1971,7 @@ function handleBeforeInput(e: InputEvent) {
 
 /* 新增：编辑模式下，允许 textarea 无限增高 */
 .note-editor-reborn.editing-viewport .editor-textarea {
-  max-height:75dvh;
+ max-height: var(--editor-max-h-edit, 75dvh);
 }
 
 /* tag 面板样式增强 */
