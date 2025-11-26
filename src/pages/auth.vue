239b4a21e82811b0bfb81aeca85405cb2222f382
woabc <yjhy88@gmail.com>
2025-11-26 10:04:28 -0800

    调整+按钮上移

--- src/pages/auth.vue
+++ src/pages/auth.vue
@@ -3133,7 +3133,7 @@ function onCalendarUpdated(updated: any) {
 /* ++ 新增：“回到顶部”按钮的样式 ++ */
 .scroll-top-button {
   position: fixed;
-  bottom: 128px;
+  bottom: 158px;
   right: 20px;
   z-index: 5000;
 
@@ -3206,7 +3206,7 @@ function onCalendarUpdated(updated: any) {
 .fab-add {
   position: fixed;
   right: 20px;
-  bottom: 40px;
+  bottom: 60px;
   z-index: 5000;
 
   width: 48px;
