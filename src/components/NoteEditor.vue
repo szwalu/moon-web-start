4d363e834af819e9800e70c09504437f73cc5ed4
woabc <yjhy88@gmail.com>
2025-12-27 15:27:46 -0800

    测试输入框初始高度

--- src/components/NoteEditor.vue
+++ src/components/NoteEditor.vue
@@ -2682,7 +2682,7 @@ function handleBeforeInput(e: InputEvent) {
   background-color: #f9f9f9;
 
   /* 1. 默认高度（新建模式） */
-  height: 56vh;
+  height: 430px;
 
   /* 2. 依然保留这个上限，防止在超大屏幕过高 */
   max-height: 100dvh;
