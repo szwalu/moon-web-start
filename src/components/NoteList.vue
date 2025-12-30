bfa6485e744cdc29c49a5edbfeb5d1dde90a2b22
woabc <yjhy88@gmail.com>
2025-12-30 14:59:30 -0800

    回到顶部按钮动画

--- src/components/NoteList.vue
+++ src/components/NoteList.vue
@@ -1069,7 +1069,8 @@ async function restoreScrollIfNeeded() {
       v-show="!isEditingTop"
       ref="scrollerRef"
       :items="mixedItems"
-      :min-item-size="120"
+      :min-item-size="26"
+      :buffer="400"
       class="scroller"
       key-field="vid"
     >
